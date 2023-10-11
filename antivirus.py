fea18a75c77f80a4a89379dd9db455ad25740b92
Tomas <73959718+tywe98@users.noreply.github.com>
2023-10-12 01:16:31 +0200

    finish check()

--- antivirus.py
+++ antivirus.py
@@ -2,10 +2,30 @@ import sys
 import base64
 from Crypto.Cipher import AES
 
+def extract_payload_version(var):
+    result = var.split('=')
+    return (int(result[1].strip())+1)
+
 
 def check(filename):
     # Your solution.
-    return None
+    lines = []
+    with open(filename, "r") as file:
+        for line in file:
+            lines.append(line.strip())
+            
+    if(len(lines) < 3):
+        #for only payload files    
+        return extract_payload_version(lines[0])
+    elif (len(lines) > 2):
+        code_locals = {}
+        for i in range(7):
+            exec(lines[i], globals(), code_locals)
+            
+        payload = code_locals['c2'].strip().split('\n')
+        return extract_payload_version(payload[0])
+    else:
+        return None
     
 
 if __name__ == '__main__':
