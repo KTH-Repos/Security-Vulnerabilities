c97ad992006d2d2089be1b0a0319416f593eaaa8
Tomas <tywe@kth.se>
2023-10-22 12:04:13 +0200

    #grading

--- filter.py
+++ filter.py
@@ -4,9 +4,13 @@ def filter_text(text):
     # Your solution
     lines = text.splitlines()
     rules = [s for s in lines if "#" in s]
+    counter = 0
     for rule in rules:
         words = rule.split(" ")
         key = words[1]
-        if len(key) < 2:
-            return False
-    return True
+        if len(key) > 1:
+            counter = counter + 1
+    if counter == len(rules):
+        return True
+    
+    
