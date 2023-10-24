9b7865792e55386744f205412e466ab5a883cb6c
Tomas <tywe@kth.se>
2023-10-24 14:14:15 +0200

    #grading

--- filter.py
+++ filter.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 
+def extractRules(rules):
+    result = {}
+    for rule in rules:
+        rule = rule.strip('#').strip()
+        key, value = rule.split('=')
+        key = key.strip()
+        value = value.strip()
+        result[key] = value
+    return result
+
 def filter_text(text):
     # Your solution
     lines = text.splitlines()
     rules = [s for s in lines if "#" in s]
-    if len(rules) == 0:
-        return True
-    counter = 0
-    for rule in rules:
-        words = rule.split(" ")
-        key = words[1]
-        if len(key) > 1:
-            counter = counter + 1
-    if counter == len(rules):
-        return True
-    else:
-        return False
-    
+    extractedRules = extractRules(rules)
+    for key in extractedRules:
+        for value in extractedRules:
+            if key == value:
+                return False
+    return True
